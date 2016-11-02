56cc49c949419e26bf05c17e61b0e1f1afc95b70
alfred <alfred@nurd.nu>
2016-11-02 23:19:33 +0000

    script for deploying to s3 and invalidating cloudfront cache

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Requires aws cli tool and credentials to be installed and configured.
+
+if [ "$1" = "" ]; then
+  >&2 echo "First argument should be the s3 bucket."
+  exit 1
+fi
+
+if [ "$2" = "" ]; then
+  >&2 echo "Second argument should be the cloudfront distribution id."
+  exit 2
+fi
+
+aws s3 sync . "s3://$1/" --acl public-read --delete --exclude '.git/*' --exclude 'deploy.sh'
+aws cloudfront create-invalidation --distribution-id "$2" --paths '/*'
